f78c12f083f88f54f8146a0a7c3ec2acfa195023
tanojsaipraveen <tanojsaipraveench@outlook.com>
2020-01-04 21:05:59 +0530

    Add project files.

---
+++ WebApplication13/Controllers/DataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebApplication13.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public DataController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Data()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var Email = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            ViewBag.Id = userId;
+            ViewBag.Email = Email;
+            return View();
+        }
+    }
+}
