9707c68592d26c0e4c589ef42d5a003a71ccae8a
tanojsaipraveen <tanojsaipraveench@outlook.com>
2020-01-05 11:53:22 +0530

    Sample view created

--- WebApplication13/Controllers/SampleController.cs
+++ WebApplication13/Controllers/SampleController.cs
@@ -12,5 +12,9 @@ namespace WebApplication13.Controllers
         {
             return View();
         }
+        public IActionResult SampleDemo()
+        {
+            return View();
+        }
     }
 }
